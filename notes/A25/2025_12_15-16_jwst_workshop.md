60f165fb55796d36dd597b9c8be2f0458cc75636
Mathieu Marquis <mathieumarquis4@gmail.com>
2025-12-17 11:07:48 -0500

    Post JWST workshop

--- notes/A25/2025_12_15-16_jwst_workshop.md
+++ notes/A25/2025_12_15-16_jwst_workshop.md
@@ -15,18 +15,15 @@
 - Maybe for a summer internship:
   - Naines IRTF (type V) G to ..., trouver température de l'étoile (SIMBAD), plot du type de l'étoile vs la
   - Type spectral pondéré par le flux en croppant certaines wavelengths du spectre
-## VSFs
-- I used a normalization by the variance, but this doesn't affect the slope of the VSF. It just changes the y-axis scaling.
 ## Questions
 - Loki only gives us the total SNR fits, not per component.
 - Why do we take $z=0.0099$ when the proposal uses $z=0.0104$?
   - It seems that its not Joseph who gave the value, but Michael obtained it from somewhere.
+- Difference between $v_\mathrm{off}$ and $\Delta v$?
+- Weirdly, some S lines have offset velocities really different than the other S lines (skewed).
 ## Ideas
 - Create 3D gaussians and superimpose the stellar dispersion vs the gas dispersion (basically plot the heatmaps as 3D plots instead and add some smoothing, this aims at showing visually how the dispersion varies).
-## TODO
-- Send email to Michael to ask about the wrong chip gap shown in the fits for global optimization and a single spaxel. Since the cube is basically cropped, it seems that the "flak" file is not indexed correctly which leads to the wrong chip gap data.
-- Filament vs non-filament regions for the VSFs
-
+## Conference
 - At registering, give choices (4-5)
   - Socks
   - Deck of cards
@@ -37,3 +34,71 @@
   - Québec tea towels
   - Montreal map (online (google maps list of places) + physical)
 - Look at this with Anabel
+## VSFs
+- Intuition:
+  - Single power law on small scales $\implies$ expected from a turbulent flow. Multiple slopes/not linear $\implies$ multiple energy ejections at these scales.
+  - Shallow slope: velocities at small and large scales are similar, small scale motions are as strong as large scale motions, motions are dominated by local processes like shocks.
+  - Steep slope: not reproduced by simulations with magnetic fields and a little far from kinetic plasma simulations, it most likely comes from SMBH feedback that does not drive "volume-filling turbulence" efficiently
+## Meeting with Gary Ferland
+- It's hard to get H$_2$ really hot without destroying it.
+- The ionization parameter is the ration of star light to gas density
+- Next meeting Thursday 8 January 2 PM
+## Meeting in January with the whole team
+- Joseph summary of data reduction (including a slide on LOKI)
+- Send a When2meet for the week of January 19
+  - We want to summarize the first results
+  - Joseph will send a mailing list
+  - Send it to Julie first
+### Letter
+- Present the data
+- Connecting feedback between kpc to sub-kpc scales (completing the feedback loop)
+- Filament connecting to the rotating disk
+### Kinematics
+#### Paper 1 (Mathieu)
+- General velocity flow (velocity + vel. dispersion of all detected lines)
+- Mention lines that are not detected
+- Single components for all the lines (except pa alpha and S3)
+- Kinematics of the stars
+- P-V diagrams for gas and stars
+- Compare with MUSE, ALMA, XRISM
+- 3D plot of the velocity dispersion of stars vs gas (overlayed)
+- Approaching/receding component for Pa alpha and S(3)
+#### Paper 2 (Mathieu)
+- VSFs for all the gas vs the filaments vs non filament, add MUSE VSF
+- VSFs for the stars (fit two lines and look at the breaking scale)
+- Try with multiple components (VSFs narrow vs broad)
+- Plots of slope as a function of temperature
+- See 1st order vs 2nd order?
+- PDFs for relevant gas, also do it for MUSE
+#### Paper 3 (another student)
+- What are the stars in there
+- Maybe codirection with Étienne Artigau?
+### Lines
+#### Paper 1 (Olivia)
+- All lines that are detected and not detected (table that gives the details of each line in each region)
+- BPT type diagram with Webb vs MUSE on two axes
+- BPT S something vs S(1)
+- HST data, most important lines we see in the data
+- Excitation diagram
+- Some tests with Tremblay's paper
+## Email for Michael
+- Wrong chip gap taken for the single spaxels
+- Chip gap is too large for individual regions also
+- Ask where he got the redshift from.
+- Ask if he masks the line to fit the continuum or if the continuum and emission lines are fit simultaneously.
+- Tie the bracket lines with the narrow component of Pa alpha
+- Why is there a spike in the 32_27 spaxel (also in the initial_sum_fit spectrum)
+- Why are there grey bands at the low spaxels numbers
+- Ask for the high resolution global optimization fits
+- What is the difference between v_off and delta_v?
+- Also give the smoothed data to Michael (wait for Joseph to reduce the data to the best resolution)
+- Send to Michael the region spectra with the new resolution
+---
+- No global H2, no chip gap specific fits.
+## TODO
+- [ ] Send email to Michael to ask about the wrong chip gap shown in the fits for global optimization and a single spaxel. Since the cube is basically cropped, it seems that the "flak" file is not indexed correctly which leads to the wrong chip gap data.
+- [x] Filament vs non-filament regions for the VSFs
+- [x] Implement the Loki fit figure for all versions
+- [ ] Convert arcsecs to pc with [NED](https://www.astro.ucla.edu/~wright/CosmoCalc.html)
+### For January meeting
+- [ ] Approaching/receding component figure for Pa alpha and S(3)
--- src/tools/loki.py
+++ src/tools/loki.py
@@ -1,5 +1,5 @@
 import graphinglib as gl
-from numpy import sum as np_sum
+import numpy as np
 from astropy.io.fits import open as fits_open
 from astropy.constants import c as light_speed
 
@@ -30,7 +30,8 @@ def get_loki_line_pdfs_figure(
     hms = []
     subtitles = []
     for line in lines:
-        for suffix in ["1.flux", "1.voff", "1.fwhm"]:
+        # for suffix in ["1.flux", "1.voff", "1.fwhm"]:
+        for suffix in ["1.flux", "vpeak", "1.fwhm"]:
             hms.append(gl.Heatmap(
                 get_pdf_image_as_array(f"data/loki/{folder_name}/param_maps/lines/{line}/{line}.{suffix}.pdf"),
                 show_color_bar=False,
@@ -53,6 +54,7 @@ def get_loki_line_pdfs_figure(
 def get_loki_fit_figure(
     model_filename: str,
     spaxel_coordinates: tuple[int, int],
+    version: int = 2,
 ) -> gl.SmartFigure:
     """
     Gives a figure showing the Loki fit for a given spaxel using the specified model file.
@@ -67,31 +69,53 @@ def get_loki_fit_figure(
         .. note::
             To be consistent with Loki's output, the coordinates should start at (1, 1) for the bottom-left spaxel and
             be ordered as (x, y).
+    version : int, default=2
+        The iteration version of the results used. 2 is for the late october version and 3 is for the december version
+        with OQBr tied. 1 is not implemented.
 
     Returns
     -------
     gl.SmartFigure
         The figure showing the Loki fit for the specified spaxel as well as the data itself.
     """
+    if version not in [2, 3]:
+        raise ValueError("Only versions 2 and 3 are supported.")
     hdu = fits_open(model_filename)
     data = hdu[1].data
+    wavelength_arange = hdu[-1].data[0][0].flatten() / (1 + 0.0099)
+
+    # Building the stellar continuum
     stellar_extinction = hdu[4].data
-    stellar_continuum = hdu[7].data * stellar_extinction
+    if version == 2:
+        raw_stellar_continuum = hdu[7].data
+        gas_lines_range = range(8, 28)
+    else:
+        raw_stellar_continuum = hdu[8].data
+        polynomials_multiplicative = hdu[7].data
+        raw_stellar_continuum *= polynomials_multiplicative
+        gas_lines_range = range(9, 29)
+    stellar_continuum = raw_stellar_continuum * stellar_extinction
+
+    # Building the gas emission lines
     gas_extinction = hdu[5].data
     silicates_extinction = hdu[6].data
-    gas_lines = np_sum([hdu[i].data for i in range(8, 28)], axis=0) * gas_extinction * silicates_extinction
+    gas_lines = np.sum([hdu[i].data for i in gas_lines_range], axis=0) * gas_extinction * silicates_extinction
+
     total_model = stellar_continuum + gas_lines
 
-    wavelength_arange = hdu[28].data[0][0].flatten() / (1 + 0.0099)
+    # Convert to erg/s/cm²/sr/Hz
     hertz_conversion_factor = light_speed.to("micron/s").value / wavelength_arange
     data *= hertz_conversion_factor[:, None, None]
     total_model *= hertz_conversion_factor[:, None, None]
     stellar_continuum *= hertz_conversion_factor[:, None, None]
 
+    # Building the emission line labels and texts
     lines = [2.2235, 2.1218, 2.0338, 1.9576, 1.8920, 1.8358, 1.7880, 1.7480, 1.7147, 2.4756, 2.5001 , 2.52802, 2.55985,
              2.62688, 2.80251, 3.00387, 1.8745, 2.6259, 2.1661, 1.9451]
     names = ["S(0)", "S(1)", "S(2)", "S(3)", "S(4)", "S(5)", "S(6)", "S(7)", "S(8)", "Q(6)", "Q(7)", "Q(8)", "Q(9)",
              "O(2)", "O(3)", "O(4)", r"Pa$\alpha$", r"Br$\beta$", r"Br$\gamma$", r"Br$\delta$"]
+    name_texts = [gl.Text(line, 0.5, name, font_size=8) for line, name in zip(lines, names)]
+    line_vlines = gl.Vlines(lines, colors="gray", line_styles="dashed", line_widths=1)
 
     data_curve = gl.Curve(wavelength_arange, data[:, *spaxel_coordinates],
                           label="data", color="black")
@@ -99,25 +123,25 @@ def get_loki_fit_figure(
                            label="total model", color="#ff5d00")
     continuum_curve = gl.Curve(wavelength_arange, stellar_continuum[:, *spaxel_coordinates],
                                label="stellar continuum only", color="fuchsia")
+    error_curve = data_curve - model_curve
+    error_curve.label = "data - model"
 
-    error = data_curve - model_curve
     fig = gl.SmartFigure(
-        2,
+        3,
         x_label=r"$\lambda_\mathrm{rest}$ [$\mu$m]",
-        sub_y_labels=[r"$\nu/_\nu$ [erg s$^{-1}$ cm$^{-2}$ sr$^{-1}$]", None],
-        elements=[[
-                data_curve, model_curve, continuum_curve,
-                gl.Vlines(lines, colors="gray", line_styles="dashed", line_widths=1),
-                *[gl.Text(line, data[:, *spaxel_coordinates].max() + 0.01, name, font_size=8)
-                  for line, name in zip(lines, names)]
-            ], [
-                error,
-            ]
+        sub_y_labels=[None, r"$\nu/_\nu$ [erg s$^{-1}$ cm$^{-2}$ sr$^{-1}$]", None],
+        elements=[
+            name_texts,
+            [data_curve, model_curve, continuum_curve, line_vlines],
+            [error_curve, line_vlines],
         ],
         x_lim=(wavelength_arange.min(), wavelength_arange.max()),
         size=(14.4, 9),
         reference_labels=False,
         share_x=True,
-        height_ratios=(3, 1),
-    ).set_visual_params(use_latex=True)
+        height_ratios=(0.2, 3, 1),
+        height_padding=0,
+        general_legend=True,
+        legend_loc=(0.85, 0.8),
+    ).set_visual_params(use_latex=True).set_ticks(minor_x_tick_spacing=0.05)
     return fig
