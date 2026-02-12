b4fdc05be9a751b5ca3e950d5e4a8af83493df52
Mathieu Marquis <mathieumarquis4@gmail.com>
2026-02-12 09:16:40 -0500

    copy_with used and changed Contour parameter order

--- src/tools/plotting.py
+++ src/tools/plotting.py
@@ -44,7 +44,7 @@ def get_smoothed_contour(image: np.ndarray, gaussian_kernel_stddev: float, **kwa
         inpaintRadius=1,
         flags=cv2.INPAINT_NS,
     )
-    contour = gl.Contour(*np.mgrid[:image_copy.shape[0], :image_copy.shape[1]][::-1], smoothed_image, **kwargs)
+    contour = gl.Contour(smoothed_image, *np.mgrid[:image_copy.shape[0], :image_copy.shape[1]][::-1], **kwargs)
     return contour
 
 def get_rotated_contour(contour: gl.Contour) -> gl.Contour:
@@ -63,11 +63,12 @@ def get_rotated_contour(contour: gl.Contour) -> gl.Contour:
         A new Contour object that is the rotated version of the input.
     """
     theta = ROTATION_ANGLE_NIRSPEC * np.pi / 180
-    cont_rot = contour.copy()
     # Add 0.5 to convert from pixel center to edge-based coordinates
     x_mesh_edges, y_mesh_edges = contour.x_mesh + 0.5, contour.y_mesh + 0.5
-    cont_rot.x_mesh = np.cos(theta) * x_mesh_edges - np.sin(theta) * y_mesh_edges
-    cont_rot.y_mesh = np.sin(theta) * x_mesh_edges + np.cos(theta) * y_mesh_edges
+    cont_rot = contour.copy_with(
+        x_mesh=np.cos(theta) * x_mesh_edges - np.sin(theta) * y_mesh_edges,
+        y_mesh=np.sin(theta) * x_mesh_edges + np.cos(theta) * y_mesh_edges,
+    )
     return cont_rot
 
 def get_rotated_heatmap(heatmap: gl.Heatmap) -> gl.Heatmap:
@@ -86,13 +87,13 @@ def get_rotated_heatmap(heatmap: gl.Heatmap) -> gl.Heatmap:
         A new Heatmap object that is the rotated version of the input.
     """
     theta = ROTATION_ANGLE_NIRSPEC * np.pi / 180
-    hm_rot = heatmap.copy()
     n, m = heatmap.image.shape
     y, x = np.mgrid[:n+1, :m+1]  # grids of each cell x/y corners
 
-    hm_rot._x_coordinates = x * np.cos(theta) - y * np.sin(theta)
-    hm_rot._y_coordinates = x * np.sin(theta) + y * np.cos(theta)
-
+    hm_rot = heatmap.copy_with(
+        x_mesh=x * np.cos(theta) - y * np.sin(theta),
+        y_mesh=x * np.sin(theta) + y * np.cos(theta),
+    )
     return hm_rot
 
 def make_pv_diagram(
@@ -181,10 +182,12 @@ def make_pv_diagram(
 
     # Contours
     meshes = np.meshgrid(np.arange(pv_data.shape[1]), np.arange(pv_data.shape[0]))
-    pv_cont = gl.Contour(*meshes, pv_data, number_of_levels=9, color_map="Reds", show_color_bar=False,
-                         color_map_range=(lower_level, cmap_upper_level))
-    pv_cont_cont = pv_cont.copy()
-    pv_cont_cont.color_map = ListedColormap("k")
-    pv_cont_cont.filled = False
+    pv_cont = gl.Contour(pv_data, *meshes, levels=contour_levels, color_map="Reds", show_color_bar=False,
+                         color_map_range=(contour_levels[0], contour_levels[-1]))
+    pv_cont_cont = pv_cont.copy_with(
+        color_map=ListedColormap("k"),
+        filled=False,
+        line_widths=0.5,
+    )
 
     return aperture_poly, bin_polygons, pv_hm, pv_cont, pv_cont_cont
