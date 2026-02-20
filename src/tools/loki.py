5c403fdcff8a7bba497fe09b6d4525e9edc7d985
Mathieu Marquis <mathieumarquis4@gmail.com>
2026-02-20 12:06:30 -0500

    loki.get_loki_gaussian_params, individual gaussian components plotted

--- src/tools/loki.py
+++ src/tools/loki.py
@@ -3,13 +3,14 @@ import numpy as np
 from astropy.io.fits import open as fits_open
 from astropy.constants import c as light_speed
 from typing import Literal
+from pandas import read_csv
 
 from src.tools.miscellaneous import get_pdf_image_as_array
 from src.config import REDSHIFT
 
 
-def get_models(
-    results_version: Literal["december lr", "february lr", "february hr"] = "december lr",
+def get_loki_models(
+    results_version: Literal["december lr", "february lr", "february hr"] | str = "december lr",
     convert_units: bool = False,
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
@@ -18,8 +19,9 @@ def get_models(
 
     Parameters
     ----------
-    results_version : Literal["december lr", "february lr", "february hr"], default="december lr"
-        The version of the Loki results to use for the stellar continuum subtraction and model retrieval.
+    results_version : Literal["december lr", "february lr", "february hr"] | str, default="december lr"
+        The version of the Loki results to use for the stellar continuum subtraction and model retrieval. If a string is
+        provided that does not match one of the specified versions, it must be the path toward a Loki model FITS file.
     convert_units : bool, default=False
         Whether to convert the units of the data and models to erg/s/cm²/sr/Hz.
 
@@ -29,6 +31,8 @@ def get_models(
         A tuple containing (data cube, wavelength array, total model cube, stellar continuum cube). The wavelength
         array is in the rest frame and has been corrected for redshift.
     """
+    prefix = "data/loki/output_NGC4696_G235H_F170LP_"
+    suffix = "_full_model.fits"
     match results_version:
         case "december lr":
             path = "full_OQBr_tied/NGC4696_G235H_F170LP_full_OQBr_tied"
@@ -37,12 +41,11 @@ def get_models(
         case "february hr":
             path = "QOBr_tied_global_m1_hires/NGC4696_G235H_F170LP_QOBr_tied_global_m1_hires"
         case _:
-            raise ValueError("Invalid results version. Choose from 'december lr', 'february lr', or 'february hr'.")
+            path = results_version
+            prefix, suffix = "", ""
 
-    prefix = "data/loki/output_NGC4696_G235H_F170LP_"
-    suffix = "_full_model.fits"
     loki_hdus = fits_open(f"{prefix}{path}{suffix}")
-    data = loki_hdus[1].data
+    data = loki_hdus[1].data  # units are erg/Hz/cm²/s/sr
     wavelength_arange = loki_hdus[-1].data[0][0].flatten() / (1 + REDSHIFT)
 
     # Building the stellar continuum
@@ -60,13 +63,69 @@ def get_models(
     total_model = stellar_continuum + gas_lines
 
     if convert_units:
-        # Convert to erg/s/cm²/sr/Hz
+        # Convert to erg/s/cm²/sr (remove 1/Hz)
         hertz_conversion_factor = light_speed.to("micron/s").value / wavelength_arange
         data *= hertz_conversion_factor[:, None, None]
         total_model *= hertz_conversion_factor[:, None, None]
         stellar_continuum *= hertz_conversion_factor[:, None, None]
 
     return data, wavelength_arange, total_model, stellar_continuum
+
+def get_loki_gaussian_params(
+    csv_filename: str,
+    lines: list[str],
+    lambda_0s: list[float],
+) -> np.ndarray:
+    """
+    Gives the Gaussian parameters (amplitude, lambda center, and sigma) for the specified lines from a Loki output CSV
+    file. This allows to plot individual Gaussian components of the fit.
+
+    Parameters
+    ----------
+    csv_filename : str
+        The path toward the Loki output CSV file containing the fit parameters. This must be one of the CSV files in the
+        "output_tables" directory of a Loki results folder.
+    lines : list[str]
+        The list of spectral lines to extract the Gaussian parameters for. These should correspond to the names and
+        index of the line in LOKI format (e.g., "HI_Pa_alpha.1" for the first Gaussian component of the Pa alpha line).
+    lambda_0s : list[float]
+        The rest wavelengths of the lines in microns. This is needed to convert the velocity parameters (voff and fwhm)
+        to wavelength units so that the output array can be used to plot the Gaussian components in wavelength space.
+
+    Returns
+    -------
+    np.ndarray
+        A 2D array of shape (len(lines), 3) containing the Gaussian parameters for each line in the order (amplitude,
+        lambda center, sigma). The amplitude is in units of erg/s/cm²/μm/sr and both the lambda center and sigma are in
+        microns.
+    """
+    if len(lines) != len(lambda_0s):
+        raise ValueError("The number of lines must match the number of rest wavelengths provided.")
+
+    # Open the CSV file as a pandas dataframe
+    df = read_csv(csv_filename, delimiter="\t")
+    df.columns = df.columns.str.strip()
+    df["name"] = df["name"].str.strip()
+    df = df.set_index("name")
+
+    light_speed_microns = light_speed.to("micron/s").value
+    light_speed_km = light_speed.to("kilometer/s").value
+
+    gaussian_params = []
+    for line, lambda_0 in zip(lines, lambda_0s):
+        amp = 10**float(df.loc[f"lines.{line}.amp", "best"])  # erg/Hz/cm²/s/sr
+        print(amp)
+        voff = float(df.loc[f"lines.{line}.voff", "best"])  # km/s
+        fwhm = float(df.loc[f"lines.{line}.fwhm", "best"])  # km/s
+
+        amp_lambda = (light_speed_microns / lambda_0**2) * amp  # erg/s/cm²/μm/sr
+        print(amp_lambda)
+        lambda_center = lambda_0 * (1 + voff / light_speed_km)  # microns
+        sigma_lambda = lambda_0 * (fwhm / light_speed_km) / (2 * np.sqrt(2 * np.log(2)))  # microns
+
+        gaussian_params.append((amp_lambda, lambda_center, sigma_lambda))
+
+    return np.array(gaussian_params)
 
 def get_loki_grid_pdfs_figure(
     lines: list[str],
@@ -114,7 +173,7 @@ def get_loki_grid_pdfs_figure(
     return fig
 
 def get_loki_fit_figure(
-    results_version: Literal["december lr", "february lr", "february hr"],
+    results_version: Literal["december lr", "february lr", "february hr"] | str,
     spaxel_coordinates: tuple[int, int],
 ) -> gl.SmartFigure:
     """
@@ -122,23 +181,22 @@ def get_loki_fit_figure(
 
     Parameters
     ----------
-    model_filename : str
-        The path to the Loki model FITS file.
+    results_version : Literal["december lr", "february lr", "february hr"] | str
+        The version of the Loki results to use for the stellar continuum subtraction and model retrieval. If a string is
+        provided that does not match one of the specified versions, it must be the path toward a Loki model FITS file.
     spaxel_coordinates : tuple[int, int]
         The (y, x) coordinates of the spaxel to visualize.
 
         .. note::
             To be consistent with Loki's output, the user may use the `FitsCoords` class to specify coordinates in
             (x, y) format and starting at (1, 1).
-    results_version : Literal["december lr", "february lr", "february hr"]
-        The version of the Loki results to use to retrieve the data and models for the fit visualization.
 
     Returns
     -------
     gl.SmartFigure
         The figure showing the Loki fit for the specified spaxel as well as the data itself.
     """
-    data, wavelength_arange, total_model, stellar_continuum = get_models(results_version, convert_units=True)
+    data, wavelength_arange, total_model, stellar_continuum = get_loki_models(results_version, convert_units=True)
 
     # Building the emission line labels and texts
     lines = [2.2235, 2.1218, 2.0338, 1.9576, 1.8920, 1.8358, 1.7880, 1.7480, 1.7147, 2.4756, 2.5001 , 2.52802, 2.55985,
@@ -176,3 +234,5 @@ def get_loki_fit_figure(
         legend_loc=(0.85, 0.8),
     ).set_visual_params(use_latex=True).set_ticks(minor_x_tick_spacing=0.05)
     return fig
+
+get_
